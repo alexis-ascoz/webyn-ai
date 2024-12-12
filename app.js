b264f47852d51d6ee5a2128a7fe71e6c861756b5
Ascoz <alexisbernard34310@gmail.com>
2024-12-12 22:57:28 +0100

    feat: update message of hello route

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello, World!' });
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello, Webyn!" });
 });
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
 });
 
 app.listen(port, () => {
